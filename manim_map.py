83c8611613ed09140d9382ff1cf5a477201fc185
Minh Nguyen <minhnguyen@Bigger-hotd0g.local>
2024-08-01 14:17:17 +0700

    3107

--- manim_map.py
+++ manim_map.py
@@ -4,8 +4,7 @@ import geopandas as gpd
 import numpy as np
 import matplotlib.pyplot as plt
 from colormap import rgb2hex
-from shapely import wkt, envelope
-from shapely import buffer
+from shapely import wkt, envelope, buffer, geometry
 import ast
 
 df = pd.read_csv("./cleaned/vietnam_adm_lvl2.csv", index_col=0)
@@ -20,88 +19,80 @@ regions_df['geometry'] = regions_df['geometry'].apply(lambda x: wkt.loads(x))
 regions_gdf = gpd.GeoDataFrame(regions_df, geometry='geometry')
 regions_gdf['coords'] = regions_gdf['coords'].apply(ast.literal_eval)
 
-#select_area
+#scaling to avoid geom overlap
+def resize_shapely_polygon(my_polygon, factor: float, swell=False):
+    xs = list(my_polygon.exterior.coords.xy[0])
+    ys = list(my_polygon.exterior.coords.xy[1])
+    x_center = .5*min(xs)+.5*max(xs)
+    y_center = .5*min(ys)+.5*max(ys)
+    min_corner = geometry.Point(min(xs), min(ys))
+    max_corner = geometry.Point(max(xs), max(ys))
+    center = geometry.Point(x_center, y_center)
+    shrink_distance = center.distance(min_corner)*factor
+    shrink_distance = shrink_distance if shrink_distance > 0.002 else 0.002
+    #print(shrink_distance)
+    if swell:
+        my_polygon_resized = my_polygon.buffer(shrink_distance) #expand
+    else:
+        my_polygon_resized = my_polygon.buffer(-shrink_distance) #shrink
+    if my_polygon_resized.geom_type == "MultiPolygon":
+        my_polygon_resized = max(my_polygon_resized.geoms, key=lambda p: p.area)
+    return my_polygon_resized
+
+
 select_regions = ["HA NOI"]
 query = "population"
 regions_gdf = regions_gdf[regions_gdf['region'].isin(select_regions)].sort_values(by=query, ascending=False)
-#regions_gdf = regions_gdf.sort_values(by=query, ascending=False)
+regions_gdf['geometry_scaled'] = regions_gdf['geometry'].apply(lambda x: resize_shapely_polygon(x, 0.05))
 len_df = len(regions_gdf)
 #print(regions_gdf)
 
 #get_boundaries
-big_polygon = envelope(regions_gdf.union_all())
+big_polygon = envelope(regions_gdf['geometry_scaled'].union_all())
 polygon_border_xy = np.array(big_polygon.boundary.coords)
 min_x = min(polygon_border_xy[:,0])
 max_x = max(polygon_border_xy[:,0])
 min_y = min(polygon_border_xy[:,1])
 max_y = max(polygon_border_xy[:,1])
-buff_x = (max_x-min_x)/2
-buff_y = (max_y-min_y)/2
+u_center = np.array([.5*max_x+.5*min_x, .5*max_y+.5*min_y, 0])
 
 # Color Mapping Setup'
 dfac = 2*10**5
 cmap = plt.get_cmap('plasma')
-norm = plt.Normalize(regions_gdf[query].quantile(0.05)/dfac, regions_gdf[query].quantile(0.98)/dfac)
-
-def shrink_or_swell_shapely_polygon(my_polygon, factor=0.10, swell=False):
-    ''' returns the shapely polygon which is smaller or bigger by passed factor.
-        If swell = True , then it returns bigger polygon, else smaller '''
-    from shapely import geometry
-
-    #my_polygon = mask2poly['geometry'][120]
-
-    shrink_factor = 0.10 #Shrink by 10%
-    xs = list(my_polygon.exterior.coords.xy[0])
-    ys = list(my_polygon.exterior.coords.xy[1])
-    x_center = 0.5 * min(xs) + 0.5 * max(xs)
-    y_center = 0.5 * min(ys) + 0.5 * max(ys)
-    min_corner = geometry.Point(min(xs), min(ys))
-    max_corner = geometry.Point(max(xs), max(ys))
-    center = geometry.Point(x_center, y_center)
-    shrink_distance = center.distance(min_corner)*0.10
-
-    if swell:
-        my_polygon_resized = my_polygon.buffer(shrink_distance) #expand
-    else:
-        my_polygon_resized = my_polygon.buffer(-shrink_distance) #shrink
-    return my_polygon_resized
+norm = plt.Normalize(regions_gdf[query].quantile(0.05)/dfac, regions_gdf[query].quantile(0.99)/dfac)
 
 # Extract coordinates from a Polygon geometry, updated to handle Multigon
 def get_line_coord(polygon) -> list:
     if polygon.geom_type == "MultiPolygon":
         p_list = list(polygon.geoms)
-        p_list = [buffer(p, 0.01) for p in p_list]
         border_xy = [np.array(p.boundary.coords) for p in p_list]
     else:
-        polygon = buffer(polygon, 0.01)
-        border_xy = [np.array(polygon.boundary.coords)]
+        border_xy =[np.array(polygon.boundary.coords)]
     return border_xy
 
 # Create line and polygon (area) with color based on population density
-def create_Polyhedron(axes, border_xy, h_tracker):
-    height = 0
+def create_Polyhedron(axes, border_xy, d_height, h_tracker):
+    height = d_height
     color = cmap(norm(height))
     hex_color = rgb2hex(*color)
     
     # Create polygon area
     points = axes.c2p(border_xy)
-    z_level = min([y for x, y, z in points]) + 3
-    top_face = Polygon(*[point + np.array([0, 0, height]) for point in points], fill_opacity=1, color=hex_color, stroke_width=1).set_shade_in_3d(True)
+    top_face = Polygon(*[point + np.array([0, 0, height]) for point in points], fill_opacity=1, color=hex_color, stroke_width=1)
     top_face.add_updater(lambda p, ht=h_tracker: p.become(
-        Polygon(*[point + np.array([0, 0, h_tracker.get_value()]) for point in points], fill_opacity=1, color=rgb2hex(*cmap(norm(ht.get_value()))), stroke_width=1).set_shade_in_3d(True)
+        Polygon(*[point + np.array([0, 0, h_tracker.get_value()]) for point in points], fill_opacity=1, color=rgb2hex(*cmap(norm(ht.get_value()))), stroke_width=1)
     ))
-    bottom_face = Polygon(*points, fill_opacity=1, color=hex_color, stroke_width=1).set_shade_in_3d(True)
+    bottom_face = Polygon(*points, fill_opacity=1, color=hex_color, stroke_width=1)
     bottom_face.add_updater(lambda p, ht=h_tracker: p.become(
-        Polygon(*points, fill_opacity=1, color=rgb2hex(*cmap(norm(ht.get_value()))), stroke_width=1).set_shade_in_3d(True)
+        Polygon(*points, fill_opacity=1, color=rgb2hex(*cmap(norm(ht.get_value()))), stroke_width=1)
     ))
     top_center = top_face.get_center()
 
     return top_face, bottom_face, top_center
-
-#need additional fixes
-def calc_side_faces(polygon, h_tracker):
-    height = 0
-    color = cmap(norm(0))
+#side_faces bw top_p and bottom_p
+def calc_side_faces(polygon, d_height, h_tracker):
+    height = d_height
+    color = cmap(norm(height-.2))
     hex_color = rgb2hex(*color)
     side_faces = VGroup()
     points = polygon.get_vertices()
@@ -120,7 +111,6 @@ def calc_side_faces(polygon, h_tracker):
             points[i] + np.array([0, 0, ht.get_value()])], fill_opacity=1, color=rgb2hex(*cmap(norm(ht.get_value()-.2))), stroke_width=1)
         ))
         side_faces.add(side_face)
-        side_faces.set_shade_in_3d(True)
     return side_faces
 
 
@@ -130,10 +120,11 @@ def calc_side_faces(polygon, h_tracker):
 class Animate_pop(ThreeDScene):
     def construct(self):
         self.camera.background_color = BLACK
-        self.set_camera_orientation(phi=30 * DEGREES, theta=-100 * DEGREES)
-        self.camera.shading_factor = .3
-        self.camera.default_distance = 20
-        self.camera.set_focal_distance(10000)
+        self.set_camera_orientation(phi=40 * DEGREES, theta=-100* DEGREES)
+        self.camera.frame_center = [0, 0, 0]
+        self.camera.shading_factor = .2
+        self.camera.default_distance = 200
+        #self.camera.set_focal_distance(10000)
         #self.begin_ambient_camera_rotation(rate=30*DEGREES, about='theta')
         #print(self.camera.get_position())
         axes = ThreeDAxes(
@@ -141,7 +132,7 @@ class Animate_pop(ThreeDScene):
             y_range=[min_y, max_y],
             z_range=[0, 4],
             axis_config={"color": WHITE},
-        ).scale(.3).shift(DOWN*2)
+        ).scale(.3).center()
         x_label = Tex("x").next_to(axes.x_axis.get_end(), RIGHT)
         y_label = Tex("y").next_to(axes.y_axis.get_end(), UP)
         self.add(axes)
@@ -150,19 +141,33 @@ class Animate_pop(ThreeDScene):
         count = 0
         animations = []
         for idx, row in regions_gdf.sort_values(by='coords', ascending=False).iterrows(): #
-            geometry = row.geometry
+            geometry = row['geometry_scaled']
+            dist_name = row['dist']
             height_goal = row[query]/dfac
-            h_tracker = ValueTracker(height_goal)
+            d_height = height_goal
+            h_tracker = ValueTracker(0)
             count += 1
             border_xy = get_line_coord(geometry)
             for bor_xy in border_xy:
-                region_topface, region_bottomface, center = create_Polyhedron(axes, bor_xy, h_tracker)
-                side_faces = calc_side_faces(region_bottomface, h_tracker)
-                polyh = VGroup(region_bottomface, side_faces, region_topface)
+                region_topface, region_bottomface, center = create_Polyhedron(axes, bor_xy, d_height, h_tracker)
+                side_faces = calc_side_faces(region_bottomface, d_height, h_tracker)
+                polyh = VGroup(region_bottomface, side_faces, region_topface).set_shade_in_3d(True)
+                if row[query] >= regions_gdf[query].quantile(0.8):
+                    label_t = Text(f"{dist_name}", 
+                                font="montserrat", 
+                                font_size=10).next_to(region_topface.get_center(), UP).set_z_index(1)
+                    background = Rectangle(width=label_t.width + 0.2, 
+                                        height=label_t.height + 0.2, 
+                                        fill_color=BLACK, fill_opacity=0.8, 
+                                        color=BLACK, 
+                                        stroke_width=0).move_to(label_t.get_center()).set_z_index(0)
+                    label = VGroup(label_t, background).rotate(PI/2, axis=RIGHT)
+                    label.add_updater(lambda mob: mob.move_to(region_topface.get_center(), UP))
+                    #print(self.camera.is_in_frame(background))
+                    self.add(label)
+                    animations.append(Create(label))
                 self.add(polyh)
-                #animations.append(h_tracker.animate.set_value(height_goal))
+                animations.append(h_tracker.animate.set_value(height_goal))
         
-        #self.play(LaggedStart(*animations, lag_ratio=0.3), rate_func=rate_functions.ease_in_out_cubic, run_time=8)
-
-
-        #self.wait(3)
+        self.play(LaggedStart(*animations, lag_ratio=0.1), rate_func=rate_functions.ease_in_out_cubic, run_time=2)
+        #self.wait(0)
